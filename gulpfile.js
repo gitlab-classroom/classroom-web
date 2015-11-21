fa63f6c73f69e9190fa729c4c1c8cfced57ba3a0
htc <htc550605125@gmail.com>
2015-11-21 14:53:32 +0800

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -6,22 +6,24 @@ var source = require('vinyl-source-stream');
 
 gulp.task('webserver', function() {
   gulp.src('app')
-    .pipe(webserver({
-      livereload: true,
-      directoryListing: true,
-      open: true
-    }));
+  .pipe(webserver({
+    livereload: true,
+    directoryListing: true,
+    open: true
+  }));
 });
 
-gulp.task('compile', function() {
-  gulp.src('src/**/*.cjsx')
-    .pipe(cjsx({bare: true}).on('error', function(e) {console.log(e)}))
-    .pipe(gulp.dest('./dist/'));
+gulp.task('compile', function(done) {
+  return gulp.src('src/**/*.cjsx')
+  .pipe(cjsx({bare: true}).on('error', function(e) {console.log(e)}))
+  .pipe(gulp.dest('./dist/'))
 })
 
-gulp.task('browserify', function() {
-    return browserify('./dist/index.js')
-        .bundle()
-        .pipe(source('bundle.js'))
-        .pipe(gulp.dest('dist'));
+gulp.task('browserify', ['compile'], function(done) {
+  return browserify('./dist/index.js')
+  .bundle()
+  .pipe(source('bundle.js'))
+  .pipe(gulp.dest('dist'))
 });
+
+gulp.task('default', ['browserify']);

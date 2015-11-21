707fc7c0f986c2254da493d3fb9cc5e520d708c6
KochiyaOcean <root@kochiyaocean.org>
2015-11-21 14:38:11 +0800

    Add browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var webserver = require('gulp-webserver');
 var cjsx = require('gulp-cjsx');
+var browserify = require('browserify')
+var source = require('vinyl-source-stream');
 
 gulp.task('webserver', function() {
   gulp.src('app')
@@ -16,3 +18,10 @@ gulp.task('compile', function() {
     .pipe(cjsx({bare: true}).on('error', function(e) {console.log(e)}))
     .pipe(gulp.dest('./dist/'));
 })
+
+gulp.task('browserify', function() {
+    return browserify('./dist/index.js')
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest('dist'));
+});

5a01aba7d96cd58ce1c580021679e226fa6021d8
htc <htc550605125@gmail.com>
2016-12-20 15:06:48 +0800

    add zh language

--- app/app.js
+++ app/app.js
@@ -105,6 +105,7 @@ if (!window.Intl) {
     .then(() => Promise.all([
       System.import('intl/locale-data/jsonp/en.js'),
       System.import('intl/locale-data/jsonp/de.js'),
+      System.import('intl/locale-data/jsonp/zh.js'),
     ]))
     .then(() => render(translationMessages))
     .catch((err) => {

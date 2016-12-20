4962c99cc05ed5c05ff6249109266400382c65a3
htc <htc550605125@gmail.com>
2016-12-20 15:37:11 +0800

    add material ui

--- app/app.js
+++ app/app.js
@@ -32,6 +32,11 @@ import LanguageProvider from 'containers/LanguageProvider';
 import 'sanitize.css/sanitize.css';
 import './global-styles';
 
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
 // Observe loading of Open Sans (to remove open sans, remove the <link> tag in
 // the index.html file and this observer)
 const openSansObserver = new FontFaceObserver('Open Sans', {});
